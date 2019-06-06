7ae68b4bde135cd63b14c3c92cf8624525a37c03
Bonwook Koo <bnoo1333@gmail.com>
2019-06-06 15:49:19 +0900

    initial commit

---
+++ app/src/main/java/com/bonoogi/darktheme/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bonoogi.darktheme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
